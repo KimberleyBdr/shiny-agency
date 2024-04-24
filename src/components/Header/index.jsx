2548c68d42134616da7ba3dc90bf2b3a6ef3c484
Kimberley Bordier <bdrkim41600@gmail.com>
2024-04-24 10:58:02 +0200

    exo 2 finish

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,30 +1,26 @@
+
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
-import colors from '../../utils/style/colors'
-import logo from '../../assets/logo.png'
+import { StyledLink } from '../../utils/style/Atoms'
+import DarkLogo from '../../assets/dark-logo.png'
 
-const StyledNav = styled.nav `
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    height: 100px;
+const HomeLogo = styled.img`
+  height: 70px;
 `
-const StyledLink = styled(Link)`
-  padding: 15px;
-  color: #8186a0;
-  text-decoration: none;
-  font-size: 18px;
-  ${(props) =>
-    props.$isFullLink &&
-    `color: white; border-radius: 30px; background-color: ${colors.primary};`}
+
+const NavContainer = styled.nav`
+  padding: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `
 
 function Header() {
   return (
-    <StyledNav>
-      <div>
-        <img src={logo} alt="logo shiny" />
-      </div>
+    <NavContainer>
+      <Link to="/">
+        <HomeLogo src={DarkLogo} />
+      </Link>
       <div>
         <StyledLink to="/">Accueil</StyledLink>
         <StyledLink to="/freelances">Profils</StyledLink>
@@ -32,7 +28,7 @@ function Header() {
           Faire le test
         </StyledLink>
       </div>
-    </StyledNav>
+    </NavContainer>
   )
 }
 
